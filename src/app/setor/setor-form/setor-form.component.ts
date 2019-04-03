02ca753ec73af193210ac114164b29651ff98820
PatrickGmailHub <pgo.guedes@gmail.com>
2019-04-03 15:41:15 -0300

    criação do formulário Setor

--- src/app/setor/setor-form/setor-form.component.ts
+++ src/app/setor/setor-form/setor-form.component.ts
@@ -1,4 +1,9 @@
+import { SetorService } from './../shared/setor.service';
+import { Setor } from './../shared/setor';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-setor-form',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetorFormComponent implements OnInit {
 
-  constructor() { }
+  isNew: boolean = true;
+  setor: Setor;
+  setores: Setor[];
+  inscricao: Subscription;
+
+  constructor(
+    private setorService: SetorService,
+    private route: ActivatedRoute,
+    private location: Location
+    ) { }
 
   ngOnInit() {
+    
+    this.inscricao = this.route.params.subscribe((params: Params) => {
+      let id: number = params['id'];
+      this.setorService.getSetor(id).toPromise()
+        .then((setor: Setor) => {
+          //console.log(setor);
+          this.setor = setor;
+        });
+    });
+
+  }
+
+  onSubmit(): void {
+    if(this.isNew) {
+
+    } else {
+
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
   }
 
 }
--- src/app/setor/shared/setor.service.ts
+++ src/app/setor/shared/setor.service.ts
@@ -1,7 +1,9 @@
 import { Setor } from './setor';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpHeaderResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+//import 'rxjs/add/operator/toPromise';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,7 @@ export class SetorService {
 
   setores: Setor[];
   setor: Setor;
+  private headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
   constructor(private http: HttpClient) { }
 
@@ -20,6 +23,18 @@ export class SetorService {
   getSetor(id: number): Observable<Setor> {
     return this.http.get<Setor>(`./../api/direct_request_v2/teste/setor/buscar/${id}`);
     //return this.getSetores().forEach(id => setor == Setor.id);
+  }
+
+  salvarSetor(setor: Setor): Observable<Setor> {
+    return this.http.post<Setor>('./../api/direct_request_v2/teste/setor/salvar', JSON.stringify(setor), {headers: this.headers});
+  }
+
+  atualizarSetor(setor: Setor): Observable<Setor> {
+    return this.http.put<Setor>('./../api/direct_request_v2/teste/setor/atualizar', setor);
+  }
+
+  deletarSetor(setor: Setor): Observable<number> {
+    return this.http.delete<number>('');
   }
 
 }
