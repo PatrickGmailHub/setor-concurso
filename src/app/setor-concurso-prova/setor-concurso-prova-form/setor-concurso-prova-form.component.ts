e490e00ca8a2f33e019390b3a29f8e60caa77afc
PatrickGmailHub <pgo.guedes@gmail.com>
2019-04-22 10:00:48 -0300

    Pequenas mudanças em setor-concurso-prova

--- src/app/setor-concurso-prova/setor-concurso-prova-form/setor-concurso-prova-form.component.ts
+++ src/app/setor-concurso-prova/setor-concurso-prova-form/setor-concurso-prova-form.component.ts
@@ -1,4 +1,9 @@
+import { LocalDeProva } from './../../shared/local-de-prova';
+import { LocalDeProvaService } from './../../shared/services/local-de-prova.service';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { SetorService } from 'src/app/shared/services/setor.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-setor-concurso-prova-form',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetorConcursoProvaFormComponent implements OnInit {
 
-  constructor() { }
+  local: LocalDeProva;
+  locais: LocalDeProva[] = [];
+  locaisDeProva: LocalDeProva[] = [];
+
+  constructor(
+    private setorService: SetorService,
+    private localDeProvaService: LocalDeProvaService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+
+    this.local = new LocalDeProva();
+
+    this.localDeProvaService.getAll().toPromise()
+      .then(locaisDeProva => {
+        this.locaisDeProva = locaisDeProva;
+        
+        this.locaisDeProva.forEach(element => {
+          delete element['version']
+          delete element['created_at']
+          delete element['updated_at']
+          delete element['deleted']
+
+        });
+
+        console.log(this.locaisDeProva);
+      });
+
   }
+
+
 
 }
