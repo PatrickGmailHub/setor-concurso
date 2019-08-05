0af7ec8bcc803c384150229e6d890d59887ebc2f
PatrickGmailHub <pgo.guedes@gmail.com>
2019-08-05 17:56:02 -0300

    Inclusão da Definição Service buscar por Concurso e Etapa Prova

--- src/app/shared/services/definicao-sala-prova.service.ts
+++ src/app/shared/services/definicao-sala-prova.service.ts
@@ -23,6 +23,10 @@ export class DefinicaoSalaProvaService {
     return this.http.get<DefinicaoSalaProva[]>(`./../api/direct_request_v2/gerencial/supervisor/distribuicao_salas/definicao_salas/buscar_por_setor`, {params: this.params.set('idSetor', `${idSetor}`)});
   }
 
+  getAllByIdSetorIdConcursoIdEtapaProva(idSetor, idConcurso, idEtapaProva):Observable<DefinicaoSalaProva[]> {
+    return this.http.get<DefinicaoSalaProva[]>(`./../api/direct_request_v2/gerencial/supervisor/distribuicao_salas/definicao_salas/buscar_por_setor_concurso_etapa_prova`, {params: this.params.append('idSetor', `${idSetor}`).append('idConcurso', `${idConcurso}`).append('idEtapaProva', `${idEtapaProva}`)});
+  }
+
   updateAll(definicaoSalaProva: DefinicaoSalaProva[]): Observable<DefinicaoSalaProva[]> {
     return this.http.put<DefinicaoSalaProva[]>('./../api/direct_request_v2/gerencial/supervisor/distribuicao_salas/definicao_salas/atualizar_multiplos', definicaoSalaProva, {headers: this.headers});
   }
